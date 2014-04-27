c6c65c8cf720bb351df4b0282639b62bcfcae7ea
Dimitur-Matev <dimmat97@gmail.com>
2014-04-27 19:18:47 +0300

    player.c and player.h

---
+++ player.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "player.h"
+#include "card.h"
+#include "deck.h"
+
+void change_lifePoints(player_t *player,int value){
+
+    player->lifePoints+=value;
+
+}
+void update_hand(player_t *player){
+    player->hand->card[player->hand->top]=pop(&player->deck);
+    player->hand->top--;
+}
+//int play_card_from_hand(player_t *player,int *i,int *l){
+//    player->hand[*i]=player->lane[*l]; //witch card on witch lane we are putting
+//    player->mana-=player->hand[*i].mana;
+//    player->hand[*i]=; // The struct is NULLed
+
+//}
