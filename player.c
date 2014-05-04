53483267c4c4fd8118b3e40082fb40844181bc2b
Dimitur-Matev <dimmat97@gmail.com>
2014-05-04 23:49:21 +0300

    .

--- player.c
+++ player.c
@@ -2,58 +2,41 @@
 #include <stdlib.h>
 #include "player.h"
 
-void init_player(struct player *p)
-{
-	p->health = 100; // could change, not sure about the value
-	mana_init(&p->manapool);
 
-	p->hand = (card_t*)malloc(NUM_OF_CARDS_IN_HAND * sizeof(card_t)); 
-
+void turn_begin(struct player *p){
 	int i;
-	for(i = 0; i < 5; ++i) // the player draws five cards from the deck
-	{
-		p->hand[i] = draw_card(&p->deck);
+	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i){
+		if(p->hand[i].name == NULL || p->hand[i].name == ""){
+			if(p->deck.top <= 0){
+				printf("No cards.\n");
+			}
+			else{
+				p->hand[i] = draw_card(&p->deck);
+			}
+			break;
+		}
 	}
 }
 
-void change_health(struct player *p, int hp, int modifier)
-{
-	if(modifier == INCREASE)
-	{
-		p->health += hp;
-	}
-	else if(modifier == DECREASE)
-	{
-		p->health -= hp;
-	}
+void print_health(struct player p){
+	printf("%d\n", p.health);
 }
 
-void print_health(struct player p)
-{
-	printf("Player health: %d\n", p.health);
-}
-
-void print_hand(struct player p)
-{
+void print_hand(struct player p){
 	int i;
-	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i)
-	{
-		if(p.hand[i].name != NULL && p.hand[i].name != "") // if the card is not "deleted" (played or whatever)
-		{
+	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i){
+		if(p.hand[i].name != NULL && p.hand[i].name != ""){
 			print_card(p.hand[i]);
 		}
 	}
 }
 
-// why should this return struct card_t ? 
-void play_card_from_hand(struct player *p, char *card_name)
-{
-	// put the card with the name "card_name" on the field
+
+void play_card_from_hand(struct player *p, char *card_name){
+
 	int i;
-	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i)
-	{
-		if(p->hand[i].name == card_name)
-		{
+	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i){
+		if(p->hand[i].name == card_name){
 			p->hand[i].name = "";
 			p->hand[i].life = 0;
 			p->hand[i].force = 0;
@@ -62,23 +45,24 @@ void play_card_from_hand(struct player *p, char *card_name)
 		}
 	}
 }
+void init_player(struct player *p){
+	p->health = 100;
+	mana_init(&p->manapool);
 
-void turn_begin(struct player *p)
-{
+	p->hand = (card_t*)malloc(NUM_OF_CARDS_IN_HAND * sizeof(card_t));
+
 	int i;
-	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i)
-	{
-		if(p->hand[i].name == NULL || p->hand[i].name == "") // finds an empty spot
-		{
-			if(p->deck.top <= 0)
-			{
-				printf("There are no more cards in the deck.\n");
-			}
-			else
-			{
-				p->hand[i] = draw_card(&p->deck);	
-			}
-			break;
-		}
+	for(i = 0; i < 5; ++i){
+		p->hand[i] = draw_card(&p->deck);
+	}
+}
+
+
+void change_health(struct player *p, int hp, int modifier){
+	if(modifier == INCREASE){
+		p->health += hp;
+	}
+	else if(modifier == DECREASE){
+		p->health -= hp;
 	}
 }
