3431ce97786d5be02eedc032fac2e9ab9792ed4f
Dimitur-Matev <dimmat97@gmail.com>
2014-05-04 23:01:09 +0300

    Final

--- player.c
+++ player.c
@@ -1,20 +1,84 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "player.h"
-#include "card.h"
-#include "deck.h"
 
-void change_lifePoints(player_t *player,int value){
+void init_player(struct player *p)
+{
+	p->health = 100; // could change, not sure about the value
+	mana_init(&p->manapool);
 
-    player->lifePoints+=value;
+	p->hand = (card_t*)malloc(NUM_OF_CARDS_IN_HAND * sizeof(card_t)); 
 
+	int i;
+	for(i = 0; i < 5; ++i) // the player draws five cards from the deck
+	{
+		p->hand[i] = draw_card(&p->deck);
+	}
 }
-void update_hand(player_t *player){
-    player->hand->card[player->hand->top]=pop(&player->deck);
-    player->hand->top--;
-}
-//int play_card_from_hand(player_t *player,int *i,int *l){
-//    player->hand[*i]=player->lane[*l]; //witch card on witch lane we are putting
-//    player->mana-=player->hand[*i].mana;
-//    player->hand[*i]=; // The struct is NULLed
 
-//}
+void change_health(struct player *p, int hp, int modifier)
+{
+	if(modifier == INCREASE)
+	{
+		p->health += hp;
+	}
+	else if(modifier == DECREASE)
+	{
+		p->health -= hp;
+	}
+}
+
+void print_health(struct player p)
+{
+	printf("Player health: %d\n", p.health);
+}
+
+void print_hand(struct player p)
+{
+	int i;
+	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i)
+	{
+		if(p.hand[i].name != NULL && p.hand[i].name != "") // if the card is not "deleted" (played or whatever)
+		{
+			print_card(p.hand[i]);
+		}
+	}
+}
+
+// why should this return struct card_t ? 
+void play_card_from_hand(struct player *p, char *card_name)
+{
+	// put the card with the name "card_name" on the field
+	int i;
+	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i)
+	{
+		if(p->hand[i].name == card_name)
+		{
+			p->hand[i].name = "";
+			p->hand[i].life = 0;
+			p->hand[i].force = 0;
+			p->hand[i].cost = 0;
+			break;
+		}
+	}
+}
+
+void turn_begin(struct player *p)
+{
+	int i;
+	for(i = 0; i < NUM_OF_CARDS_IN_HAND; ++i)
+	{
+		if(p->hand[i].name == NULL || p->hand[i].name == "") // finds an empty spot
+		{
+			if(p->deck.top <= 0)
+			{
+				printf("There are no more cards in the deck.\n");
+			}
+			else
+			{
+				p->hand[i] = draw_card(&p->deck);	
+			}
+			break;
+		}
+	}
+}
